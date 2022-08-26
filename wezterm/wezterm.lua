aefbb26c5dd386f84c19c7dcac1a892815b594a5
Anthony <anthonypoullain.pro@gmail.com>
2022-08-26 23:56:21 +0200

    :construction: Backup

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require('wezterm')
+return {
+	font = wezterm.font 'JetBrainsMono Nerd Font Mono',
+	color_scheme = "OneDark (base16)",
+	exit_behavior = 'Close',
+	enable_tab_bar = false,
+	ToggleFullScreen = false,
+	window_decorations = "RESIZE",
+}
